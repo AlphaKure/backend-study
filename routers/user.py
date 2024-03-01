e09a0821346f6e9ca5b33b110ceb2bd369153ab0
Alpha <74049718+AlphaKure@users.noreply.github.com>
2024-03-01 10:37:26 +0800

    獲得登入會員基本資料功能實現

--- routers/user.py
+++ routers/user.py
@@ -3,7 +3,7 @@ from utils.schemas import UserCreate
 from routers.auth import decode_token
 from module import UserDAL
 
-from fastapi import APIRouter,Request,Depends
+from fastapi import APIRouter,Request,Depends,HTTPException
 from fastapi.responses import JSONResponse
 from sqlalchemy.exc import IntegrityError
 
@@ -19,11 +19,11 @@ async def get_db():
             yield UserDAL(session)
 
 @route.post('/register',
-            responses={
-                200:{"description":"Request Accept","content":{"application/json":{"example":{"response":"success"}}}},
-                400:{"description":"Request Reject","content":{"application/json":{"example":{"response":"email or username used!"}}}}
-            }
-            )
+    responses={
+        200:{"description":"Request Accept","content":{"application/json":{"example":{"detail":"success"}}}},
+        400:{"description":"Request Reject","content":{"application/json":{"example":{"detail":"email or username used!"}}}}
+        }
+    )
 async def user_register(req:Request,reqBody:UserCreate,db:UserDAL=Depends(get_db)):
     """
     # 使用者註冊
@@ -40,9 +40,23 @@ async def user_register(req:Request,reqBody:UserCreate,db:UserDAL=Depends(get_db
             password= reqBody.password
         )
     except IntegrityError :
-        return JSONResponse(content= {"response":"email or username used!"}, status_code=400)
-    return JSONResponse(content= {"response":"success"}, status_code=200)
+        raise HTTPException(status_code=400,detail="Username or email used!")
+    return JSONResponse(content= {"detail":"success"}, status_code=200)
 
-@route.get('/me')
-async def get_current_user(datas:dict=Depends(decode_token)):
-    return datas.get('username')
+@route.get(
+    '/',
+    responses={
+        200:{"description":"Request Accept","content":{"application/json":{"example":{"username":"username","email":"user@email.com","level":0}}}}
+    }
+)
+async def get_current_user(datas:dict=Depends(decode_token),db:UserDAL=Depends(get_db)):
+    """
+
+    獲得目前登入帳號的基本資料
+
+    Returns:
+     - username (str) 使用者帳號
+     - email (str) 使用者電子信箱
+     - level (int) 等級
+    """
+    return await db.get_target_user_info(datas.get("username"))
